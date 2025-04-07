4df76b6b89d27a8de9de21aa398034a722080625
M Hamza Tariq <111647708+Hmmza-tariq@users.noreply.github.com>
2025-04-07 12:50:27 +0500

    07 April - Lecture

--- lib/navigation/app_router.dart
+++ lib/navigation/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
-
+import '../screens/login_screen.dart';
 import '../models/models.dart';
+import '../screens/onboarding_screen.dart';
 
 class AppRouter {
   final AppStateManager appStateManager;
@@ -17,5 +18,36 @@ class AppRouter {
       debugLogDiagnostics: true,
       refreshListenable: appStateManager,
       initialLocation: '/login',
-      routes: []);
+      routes: [
+        GoRoute(
+            name: 'login',
+            path: '/login',
+            builder: (context, state) {
+              return const LoginScreen();
+            }),
+        GoRoute(
+            name: 'onboarding',
+            path: '/onboarding',
+            builder: (context, state) {
+              return const OnboardingScreen();
+            }),
+      ],
+      redirect: (context, state) {
+        final loggedIn = appStateManager.isLoggedIn;
+        final isLoggingIn = state.matchedLocation == '/login';
+        if (!loggedIn) return isLoggingIn ? null : '/login';
+        // if (!loggedIn && !isLoggingIn) {
+        //   return '/login';
+        // }
+        final isOnboardingComplete = appStateManager.isOnboardingComplete;
+        final isOnboarding = state.matchedLocation == '/onboarding';
+        if (!isOnboardingComplete) {
+          return isOnboarding ? null : '/onboarding';
+        }
+
+        if (isLoggingIn || isOnboarding) {
+          return '/{$FooderlichTab.explore}';
+        }
+        return null;
+      });
 }
